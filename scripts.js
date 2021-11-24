52ca4ed163d2d0423077340bfbbaf46ea99c4e99
Laissa <laissaengmect@gmail.com>
2021-11-24 14:51:06 -0300

    blog

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector("#send");
+
+btn.addEventListener("click", function(e){
+    e.preventDefault()
+
+    const titulo = document.querySelector("#titulo");
+    const value = titulo.value
+    
+    const post = document.querySelectorAll("#post");
+    const valueB = post.valueB
+   
+    
+})
+alert(value)
+alert(valueB)
